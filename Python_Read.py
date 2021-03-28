241b4ff751982e4b02c0c16698cf6939860ffd2f
kirpatil1984 <81517542+kirpatil1984@users.noreply.github.com>
2021-03-28 18:49:02 +0530

    Python program to count character form the string

---
+++ Python_Read.py
@@ -0,0 +1,9 @@
+#programe to count charator form the string
+import pprint
+message = "ASASASAAAAasaasaaaaaaaaaaaaa"
+count = {}
+for char in message.upper():
+    count.setdefault(char, 0)
+    count [char] = count[char]  + 1
+
+pprint.pprint(count)

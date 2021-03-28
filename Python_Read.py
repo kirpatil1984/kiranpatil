81a81219d238b7a49f406f5b7d000c7b893cdab1
kirpatil1984 <81517542+kirpatil1984@users.noreply.github.com>
2021-03-28 18:58:56 +0530

    remove the pprint module

--- Python_Read.py
+++ Python_Read.py
@@ -1,9 +1,9 @@
 #program to count character form the string
-import pprint
+#import pprint
 message = "ASASASAAAAasaasaaaaaaaaaaaaa"
 count = {}
 for char in message.upper():
     count.setdefault(char, 0)
     count [char] = count[char]  + 1
 
-pprint.pprint(count)
+print(count)

d575371b5926472439a91f60eec5e79733021f31
kirpatil1984 <kirpatil1984@gmail.com>
2021-03-29 12:11:36 +0530

    Adding pprint module

--- Python_Read.py
+++ Python_Read.py
@@ -1,10 +1,12 @@
 #program to count character form the string
 #final
-#import pprint
+#Adding pprint
+
+import pprint
 message = "ASASASAAAAasaasaaaaaaaaaaaaa"
 count = {}
 for char in message.upper():
     count.setdefault(char, 0)
     count [char] = count[char]  + 1
 
-print(count)
+pprint.pprint(count)
